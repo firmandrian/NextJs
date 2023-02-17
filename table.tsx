dffb3b0edeb66f633b2e667e0532df12443d3763
Firman andrian <92195418+firmandrian@users.noreply.github.com>
2023-02-17 16:27:59 +0700

    update

--- table.tsx
+++ table.tsx
@@ -4,16 +4,27 @@ import  'bootstrap/dist/css/bootstrap.min.css' ;
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export default function Tabel() {
   return (
     <>
+   <Container>
+      <Row>
+        <Col style={{backgroundColor: "red"}} sm={2}>sm=8</Col>
+        <Col style={{backgroundColor: "pink"}}sm={8}>sm=4</Col>
+        <Col style={{backgroundColor: "yellow"}} sm={2}>sm=8</Col>
+      </Row>
+    </Container>
 
-     <Button className="ms-2 mb-3 mt-3" variant="primary">Add</Button>{' '} 
-     <Button className="ms-2 mb-3 mt-3" variant="danger">Delete</Button>{' '}
+
+     <Button variant="primary">Add</Button>{' '} 
+     <Button variant="danger">Delete</Button>{' '}
      <Stack direction="horizontal" gap={1}>
-      <Form.Control className="ms-2 mb-4 mt-2" placeholder="Search..." style={{width: '30%'}} />
+      <Form.Control placeholder="Search..." />
       </Stack>
-      <div className="container" style={{ maxWidth: "80%" }}>
+      <div className="container" >
     <Table striped bordered hover>
     <thead>
       <tr>
